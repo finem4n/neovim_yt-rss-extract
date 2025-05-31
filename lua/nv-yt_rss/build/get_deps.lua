3f3112c730e9b6d55fc5ae574cde456c8d5140f9
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-05-31 19:13:42 +0200

    chore(build): untest build file

--- lua/nv-yt_rss/build/get_deps.lua
+++ lua/nv-yt_rss/build/get_deps.lua
@@ -1,17 +1,14 @@
 local M = {}
 
--- return function ()
-  -- local gist_url = "https://gist.github.com/tjluoma/fdbc63ceb78a2aecd3d638fd18b6ec6e"
-  -- local gist_dir = vim.fn.stdpath("data") .. "/lazy/neovim_yt-rss-extract/lua/youtube-rss_gist"
-  --
-  -- if vim.fn.isdirectory(gist_dir) == 0 then
-  --   vim.fn.mkdir(gist_dir, "p")
-  --
-  --   local output = vim.fn.system({ 'git', 'clone', '--depth=1', gist_url, gist_dir })
-  -- end
--- end
 function M.get_deps ()
-  print("hejhej")
+  local gist_url = "https://gist.github.com/tjluoma/fdbc63ceb78a2aecd3d638fd18b6ec6e"
+  local gist_dir = vim.fn.stdpath("data") .. "/lazy/neovim_yt-rss-extract/lua/youtube-rss_gist"
+
+  if vim.fn.isdirectory(gist_dir) == 0 then
+    vim.fn.mkdir(gist_dir, "p")
+
+    local output = vim.fn.system({ 'git', 'clone', '--depth=1', gist_url, gist_dir })
+  end
 end
 
 return M
