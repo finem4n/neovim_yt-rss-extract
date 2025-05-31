51c3b5021a52e1c6334abe5bde86798afad69838
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-05-31 19:10:22 +0200

    chore(build): git clone youtube rss file

---
+++ lua/nv-yt_rss/build/get_deps.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+-- return function ()
+  -- local gist_url = "https://gist.github.com/tjluoma/fdbc63ceb78a2aecd3d638fd18b6ec6e"
+  -- local gist_dir = vim.fn.stdpath("data") .. "/lazy/neovim_yt-rss-extract/lua/youtube-rss_gist"
+  --
+  -- if vim.fn.isdirectory(gist_dir) == 0 then
+  --   vim.fn.mkdir(gist_dir, "p")
+  --
+  --   local output = vim.fn.system({ 'git', 'clone', '--depth=1', gist_url, gist_dir })
+  -- end
+-- end
+function M.get_deps ()
+  print("hejhej")
+end
+
+return M
--- lua/nv-yt_rss/init.lua
+++ lua/nv-yt_rss/init.lua
@@ -5,4 +5,10 @@ M.ns = vim.api.nvim_create_namespace("nv-yt_rss")
 function M.test ()
   print("hej")
 end
+
+function M.build ()
+  require("nv-yt_rss.build.get_deps").get_deps()
+end
+-- M.build = require"build.get_deps")
+
 return M
