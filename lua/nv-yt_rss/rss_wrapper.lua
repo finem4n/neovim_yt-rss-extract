23e5de64aac93cc0c76e6b8b97100eaf3f3f2f59
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-05-31 23:53:28 +0200

    chore(todos): update todo in rsa_wrapper.lua

--- lua/nv-yt_rss/rss_wrapper.lua
+++ lua/nv-yt_rss/rss_wrapper.lua
@@ -3,6 +3,7 @@ local utils = require("nv-yt_rss.utils")
 local M = {}
 
 function M.get_rss (youtube_link)
+  -- TODO check for nils
   local script_filepath = utils.get_plug_path() .. "youtube-rss_gist/youtube-rss.sh"
   local rss_link = vim.fn.system({script_filepath, youtube_link})
   rss_link = vim.trim(rss_link)
