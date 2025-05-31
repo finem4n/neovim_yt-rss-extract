f4745fea95586db0303aa6f28906e26e1cb22e68
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-05-31 23:17:33 +0200

    fix: trim link

--- lua/nv-yt_rss/rss_wrapper.lua
+++ lua/nv-yt_rss/rss_wrapper.lua
@@ -5,6 +5,7 @@ local M = {}
 function M.get_rss (youtube_link)
   local script_filepath = utils.get_plug_path() .. "youtube-rss_gist/youtube-rss.sh"
   local rss_link = vim.fn.system({script_filepath, youtube_link})
+  rss_link = vim.trim(rss_link)
   return rss_link
 end
 
